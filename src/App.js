c0fcc39c3c8bf677f1f9dee61915899da16a27ed
ysagar558 <ysagar558@gmail.com>
2025-12-23 16:18:46 +0530

    Token validation with firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import { Switch, Route } from "react-router-dom";
+import {useContext,useEffect} from 'react';
+import { Switch, Route, Redirect } from "react-router-dom";
 import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
 import UserProfile from './components/Profile/UserProfile';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const ctx=useContext(AuthContext);
+  useEffect(()=>{
+    const checkToken=async ()=>{
+      if(!ctx.token) return;
+      try{
+        const res=await fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyB0e7Z_UOBldjUY0i1y3N4i8t_odTfBaog',{
+          method:'POST',
+          body:JSON.stringify({idToken:ctx.token}),
+          headers:{'Content-Type':'application/json'},
+        });
+        if(!res.ok) ctx.logout();
+      }
+      catch{
+        ctx.logout();
+      }
+    };
+    checkToken();
+  },[ctx]);
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!ctx.isLoggedin && <Route path='/auth'>
           <AuthPage />
-        </Route>
-        <Route path='/profile'>
+        </Route>}
+        {ctx.isLoggedin && <Route path='/profile'>
           <UserProfile />
+        </Route>}
+        <Route path='*'>
+          <Redirect to='/'/>
         </Route>
       </Switch>
     </Layout>
